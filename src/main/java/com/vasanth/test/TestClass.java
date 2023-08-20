80fb58477c5f63b91a090141b42306c6f8633154
Vasanthpathi549 <47381837+Vasanthpathi549@users.noreply.github.com>
2023-08-20 13:40:55 +0530

    My first Java Commit

---
+++ src/main/java/com/vasanth/test/TestClass.java
@@ -0,0 +1,12 @@
+package com.vasanth.test;
+
+import java.util.List;//
+
+public class TestClass {
+    public static void main(String[] args) {
+        List<Integer> nums = List.of(1, 2, 3, 4, 5);
+
+        System.out.println("Hey Vasanth "+nums.size());
+    }
+
+}
